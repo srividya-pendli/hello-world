ea5279e6d4c9f81435998a6d12e856c509ab48b5
srividya-pendli <srividyaerukala@gmail.com>
2022-03-29 18:58:55 +0530

    Add UserReplace branch and program

--- hello world batch/Program.cs
+++ hello world batch/Program.cs
@@ -11,7 +11,7 @@ namespace hello_world_batch
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO FIRST CLASS OF RFP");
-            Console.WriteLine("1:Human\n2:SwapNumbers\n3:reverse or Palindrome");
+            Console.WriteLine("1:Human\n2:SwapNumbers\n3:reverse or Palindrome\n4:UserReplace");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -27,6 +27,9 @@ namespace hello_world_batch
                     Console.WriteLine("Enter a number to reverse");
                     int number = Convert.ToInt32(Console.ReadLine());
                     Reverse.FindReverse(number);
+                    break;
+                case 4:
+                    UserReplace.ReplaceUserName();
                     break;
             }
               Console.ReadLine();
---
+++ hello world batch/UserReplace.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world_batch
+{
+    class UserReplace
+    {
+        public static void ReplaceUserName()
+        {
+            string line = "hello <<userName>>,how are you ,what you are studing?";
+            Console.WriteLine("please enter userName");
+            string userName = Console.ReadLine();
+            //string updatedRes=line.Replace("<<userName>>",userName);
+            Console.WriteLine("after replace\n" + line.Replace("<<userName>>", userName));
+        }
+    }
+}
