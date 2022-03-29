2c07727b83387759b983a87d4446eb705b50063c
srividya-pendli <srividyaerukala@gmail.com>
2022-03-29 18:52:06 +0530

    Add palindrome branch and program

---
+++ hello world batch/Palindrome.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world_batch
+{
+    class Palindrome
+    {
+        public static void FindPalindrome(int inputNumb, int reverse)
+        {
+            if (reverse == inputNumb)
+            {
+                Console.WriteLine("given number is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("not palindrome");
+            }
+        }
+    }
+}
--- hello world batch/Program.cs
+++ hello world batch/Program.cs
@@ -11,7 +11,7 @@ namespace hello_world_batch
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO FIRST CLASS OF RFP");
-            Console.WriteLine("1:Human\n2:SwapNumbers\n3:reverse");
+            Console.WriteLine("1:Human\n2:SwapNumbers\n3:reverse or Palindrome");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
