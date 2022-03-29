4f05acef77b967de3f464b95282b0116b01f4886
srividya-pendli <srividyaerukala@gmail.com>
2022-03-29 18:08:30 +0530

    Add human branch and program

--- hello world batch/Program.cs
+++ hello world batch/Program.cs
@@ -11,7 +11,16 @@ namespace hello_world_batch
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO FIRST CLASS OF RFP");
-            Console.ReadLine();
+            Console.WriteLine("1:Human");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    human human = new human();
+                    human.Speek();
+                        break;
+            }
+              Console.ReadLine();
         }
     }
-}
+ }
---
+++ hello world batch/human.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world_batch
+{
+    class human
+    {
+        //variables
+        //access-modifier datatype varaiable name
+        public string name = "nish", address = "mdr";
+        public double height = 5.56;
+        //public decimal a = 12.45M;
+
+        //methods
+        public void Speek()
+        {
+            Console.WriteLine("he/she can talk");
+            Console.WriteLine("Name:{0} Address:{1} height:{2}", name, address, height);
+
+        }
+    }
+}
