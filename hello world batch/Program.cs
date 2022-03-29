905f0bf171b48da531063c8bcca3a3beca64729e
srividya-pendli <srividyaerukala@gmail.com>
2022-03-29 18:17:41 +0530

    SwapNumbers and program

--- hello world batch/Program.cs
+++ hello world batch/Program.cs
@@ -11,7 +11,7 @@ namespace hello_world_batch
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO FIRST CLASS OF RFP");
-            Console.WriteLine("1:Human");
+            Console.WriteLine("1:Human\n2:SwapNumbers");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -19,6 +19,10 @@ namespace hello_world_batch
                     human human = new human();
                     human.Speek();
                         break;
+                case 2:
+                    SwapNumbers swapNumbers = new SwapNumbers();
+                    swapNumbers.SwapTwoNumbers();
+                    break;
             }
               Console.ReadLine();
         }
---
+++ hello world batch/SwapNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world_batch
+{
+    class SwapNumbers
+    {
+
+        public int first, second, temp = 0;
+
+        public void SwapTwoNumbers()
+        {
+            Console.WriteLine("please enter first number");
+            string s = Console.ReadLine();
+            Console.WriteLine("please enter first number");
+            first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter second number");
+            second = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Before swap");
+            Console.WriteLine("First:{0} Second:{1}", first, second);
+            temp = first;
+            first = second;
+            second = temp;
+            Console.WriteLine("After swapped");
+            Console.WriteLine("First:{0} Second:{1}", first, second);
+        }
+    }
+}
