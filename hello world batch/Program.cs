57575a149023d315b49fa2242c9a028c5caa02ac
srividya-pendli <srividyaerukala@gmail.com>
2022-03-29 17:56:12 +0530

    first commit

---
+++ hello world batch/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world_batch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO FIRST CLASS OF RFP");
+            Console.ReadLine();
+        }
+    }
+}
